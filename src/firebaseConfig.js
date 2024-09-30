04d61b2795eb6f02fae7672f8c56357f6185913b
EducitoUwu <130678107+EducitoUwu@users.noreply.github.com>
2024-09-30 02:45:41 -0300

    .env anashei

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,17 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtxWNOMZugDHP6oDp22xzrcZaNuWSF1Y0",
-  authDomain: "ingesoquizenfermeria.firebaseapp.com",
-  projectId: "ingesoquizenfermeria",
-  storageBucket: "ingesoquizenfermeria.appspot.com",
-  messagingSenderId: "1004424951739",
-  appId: "1:1004424951739:web:355f341a58bd7e9e8af69e"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider(); // Crea una instancia del proveedor de Google
+const provider = new GoogleAuthProvider();
 
 export { auth, provider, signInWithPopup };
