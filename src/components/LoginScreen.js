2c1c66a08829036ece427d00f642836aefcc0796
EducitoUwu <130678107+EducitoUwu@users.noreply.github.com>
2024-10-14 18:40:59 -0300

    ai si

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -12,11 +12,11 @@ const LoginScreen = () => {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
 
-      if (user.email.endsWith('@ucn.cl')) {
+      if (user.email.match(/^[^@]+@[^@]+\.ucn\.cl$/)) {
         localStorage.setItem('isAuthenticated', true);
         setIsAuthenticated(true);
       } else {
-        alert('Solo se permiten correos de dominio @ucn.cl');
+        alert('Solo se permiten correos del dominio ucn.cl');
         auth.signOut();
       }
     } catch (error) {
