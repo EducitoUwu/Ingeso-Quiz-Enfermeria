d054751a323f2fb53930bd45e1ce802dac6ac126
Gilberto Muñoz Benjamín Ignacio <131167469+Benlox44@users.noreply.github.com>
2024-12-02 17:08:38 -0300

    StudyMaterial y font

--- src/components/StudyMaterial.js
+++ src/components/StudyMaterial.js
@@ -4,14 +4,24 @@ import Header from './Header';
 
 const StudyMaterial = () => {
   return (
-    <div className="study-container">
-      <Header />
-      <h1>Material de Estudio</h1>
-      <div>
-        <img src="https://example.com/material1.jpg" alt="Material de estudio 1" />
-        <p>Descripción del material de estudio 1...</p>
+    <>
+      <Header /> {/* Header fuera del contenedor principal */}
+      <div className="study-material-container">
+        <h1>Material de Estudio</h1>
+        <div>
+        <p>
+          En el siguiente enlace encontrarás un acceso directo a una carpeta de Google Drive que contiene una colección de archivos PDF con material de estudio relacionado con el cuidado de heridas. Estos recursos están diseñados para ayudarte a profundizar en el tema y mejorar tus conocimientos.
+        </p>
+          <a
+            href="https://drive.google.com/drive/folders/1oMKMEknycLH_V0UMGu3h1TiL-lx580m-?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Link a drive
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
